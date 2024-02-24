e9df6fcdd4eee02a929282cd68d7118935b9c1ff
Avijit2002 <avijitram2013@gmail.com>
2024-02-24 23:15:55 +0530

    transactions

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,69 @@ async function createTable() {
 //createTable()
 
 // Do this to avoid sql injection
+// If we directly put data coming from client in SQL string, it will be treated as SQL
+
 async function createUser(username: string, email: string) {
     const values = [username, email];
-    const query = `INSERT INTO users2 (username,email) VALUES ($1,$2);`
-    await client.query(query,values)
+
+    //const query = `INSERT INTO users2 (username,email) VALUES (${usrname},${email});` // insecure // here username can be a bad SQL query injected by hacker, 
+
+    const query = `INSERT INTO users2 (username,email) VALUES ($1,$2);` // this is secure // here even if username is SQL query, no effect
+    await client.query(query, values)
 }
-createUser('anjali',"anj@gmail.com")
+//createUser('anjali',"anj@gmail.com")
+
+
+// --- RELATIONSHIPS --- //
+
+async function createRelationTable() {
+    await client.query(`CREATE TABLE IF NOT EXISTS users3 (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+    
+    CREATE TABLE IF NOT EXISTS addresses (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        city VARCHAR(100) NOT NULL,
+        country VARCHAR(100) NOT NULL,
+        street VARCHAR(255) NOT NULL,
+        pincode VARCHAR(20),
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users3(id) ON DELETE CASCADE
+    );
+    `)
+}
+
+createRelationTable()
+client.query(`INSERT INTO users3 (username,email,password) VALUES ("avi","avi@gmail.com","123");`)
+
+client.query(`INSERT INTO addresses (user_id, city, country, street, pincode)
+VALUES (1, 'New York', 'USA', '123 Broadway St', '10001');`)
+
+client.query(`SELECT city, country, street, pincode
+FROM addresses
+WHERE user_id = 1;`)
+
+//This would require transactions  in SQL to ensure either both the user information and address goes in, or neither does
+
+
+// --- TRANSACTIONS --- //
+
+// BEGIN; 
+
+// INSERT INTO users (username, email, password)
+// VALUES ('john_doe', 'john_doe1@example.com', 'securepassword123');
+
+// INSERT INTO addresses (user_id, city, country, street, pincode)
+// VALUES (currval('users_id_seq'), 'New York', 'USA', '123 Broadway St', '10001');
+
+// COMMIT;
+
+
+// if error ROLLBACK;
+
+
